7077c97ebbfe906d03417b00fb3e9750d10dd1af
r1ddax <r1ddax00@gmail.com>
2021-04-13 23:53:34 -0300

    Instance Employee, name and code(rfid).

---
+++ README.md
@@ -0,0 +1,27 @@
+# Punch the Clock.
+##### This project want to make a product that can save and monitoring "punch the clock" in real time using hardware and software.
+
+-----------------
+
+### *Goals:*
+
+##### _D -> Doing._
+##### _F -> Finished._
+
+##### [D] - Instance object of a classe Employee.
+##### [D] - Delete intances to free memory.
+##### [ ] - Create lists and add and rm objects.
+##### [ ] - Choose RTC (real-time clock) to use.
+##### [F] - Choose LCD (16x2) to make the interface.
+##### [F] - 5 Tactile Push Button to control interface.
+##### [F] - Choose a RFID module.
+##### [ ] - Create a code to manage all components.
+##### [ ] - Choose a microcontroller and adapt the code.
+
+##### This projects need to "punch the clock" of Employee, but this project want o be easier to edit Employee Name, Employee Code... or to remove/add new Employee in the system.
+
+-----------------
+
+### *How to use:*
+
+##### This project uses the c programming language. You can clone this project and modify or run using gcc compiler.
---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// Class Employee:
+typedef struct Employee Employee;
+
+struct Employee {
+	// Parameters
+	char name[16];
+	char code[16];
+
+	// Methods
+	void (*ptrParamDefine)(Employee *);
+};
+
+// Prototypes Employee:
+void settingEmployee(Employee *obj);
+void defineParam(Employee *obj);
+void tryGetChar(char stgwrite[32], char *p);
+void print(Employee *obj);
+
+// Program:
+int main() {
+	printf("\n --- [P1] New Object --- \n");
+	Employee *P1 = (Employee *)calloc(1, sizeof(Employee));
+	settingEmployee(P1);
+
+	printf("\n --- [P2] New Object --- \n");
+	Employee *P2 = (Employee *)calloc(1, sizeof(Employee));
+	settingEmployee(P2);
+
+	printf("\n --- Objects --- \n");
+	printf(" > [P1] Name Object: %s \t\t [P2] Name Object: %s \n", P1->name, P2->name);
+	printf(" > [P1] Code Object: %s \t\t [P2] Code Object: %s \n", P1->code, P2->code);
+
+}
+
+// Functions
+
+void settingEmployee(Employee *obj) {
+    // Creating Employee and defining name/codes.
+	obj->ptrParamDefine = defineParam;
+    obj->ptrParamDefine(obj);
+}
+
+void defineParam(Employee *obj) {
+    // Call functions to define name, code and print.
+    tryGetChar(" > Write the employee NAME: ", obj->name); // Str for name.
+    tryGetChar(" > Write the employee CODE: ", obj->code); // Str for code.
+    print(obj);
+}
+
+void tryGetChar(char stgwrite[32], char *p){
+    // Function to get some char with 16 letter or less.
+	char _select = 'n';
+
+	while (_select != 'y')
+	{
+    int n; char ch;
+
+        do
+        {
+		printf("\n%s",stgwrite);
+		scanf("%15s", p);
+		for (n = 0; (ch = getchar()) != '\n' && ch != EOF; ++n);
+		if (n) printf("\n > Please try 16 chars or less");
+        } while (n);
+
+	printf("\n > You wrote <%s>, is it right? (y/n): ", p);
+	scanf(" %c", &_select);
+	_select = tolower(_select);
+	}
+
+}
+
+void print(Employee *obj) {
+    // Print name and code.
+    printf("\n > Name: [%s] | > Code: [%s]\n", obj->name, obj->code);
+}
